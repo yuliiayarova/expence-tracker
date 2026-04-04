ced6325fd765d4b7d12d4f2192a13e122cee102c
shtyktasya <stiktasa@gmail.com>
2026-04-04 19:02:03 +0300

    create welcom page

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,8 @@ import "./globals.css";
 import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 import Container from "@/components/Container/Container";
 import Button from "@/components/Button/Button";
-import { BgImageWrapper } from "@/components/BgImageWrapper/BgImageWrapper";
+
+import WelcomePage from "@/components/WellcomPage/WellcomPage";
 
 const inter = Inter({
   weight: ["400", "700"],
@@ -28,8 +29,14 @@ export default function RootLayout({
       <body className={`${inter.variable} font-sans antialiased`}>
         <TanStackProvider>
           <Container>
-            <BgImageWrapper/>
+
+           
+            <WelcomePage />
             <main>{children}</main>
+
+
+              
+
           </Container>
         </TanStackProvider>
       </body>
