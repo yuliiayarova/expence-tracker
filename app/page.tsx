9e0617d8eddbddd6de15db8b4803ca8d598a530c
vjosik <108922728+vjosik@users.noreply.github.com>
2026-04-06 17:09:36 +0300

    fix redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import WelcomePage from "@/components/WellcomPage/WellcomPage";
-import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("/transactions/expenses");
   return <WelcomePage />;
 }
