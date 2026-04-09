27a280d0926686d2830c4dd42648c37aeef7dceb
vjosik <108922728+vjosik@users.noreply.github.com>
2026-04-09 22:46:43 +0300

    fix 409

--- components/CategoriesModal/CategoriesModal.tsx
+++ components/CategoriesModal/CategoriesModal.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { toast } from 'react-hot-toast';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 import Modal from '@/components/Modal/Modal';
 import Button from '@/components/Button/Button';
@@ -116,7 +116,15 @@ export default function CategoriesModal({
         setInputValue('');
       }
     },
-    onError: error => toast.error(getErrorMessage(error)),
+    onError: (error: Error | AxiosError) => {
+      if (axios.isAxiosError(error) && error.response?.status === 409) {
+        toast.error(
+          'Cannot delete: this category is already in use by transactions.',
+        );
+      } else {
+        toast.error('Something went wrong. Please try again.');
+      }
+    },
   });
 
   const isActionsPending =
