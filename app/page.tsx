9a73fed264ade8ee01f36df7026b36d44f196de7
Yuliia Yarova <yuliiayarova96@gmail.com>
2026-04-09 12:26:48 +0300

    fix AuthProver

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,30 @@
+'use client';
+
 import WelcomePage from '@/components/WellcomPage/WellcomPage';
+import Loader from '@/components/Loader/Loader';
+import { useAuthStore } from '@/lib/store/authStore';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
+  const router = useRouter();
+
+  const isAuthenticated = useAuthStore(state => state.isAuthenticated);
+  const isAuthChecked = useAuthStore(state => state.isAuthChecked);
+
+  useEffect(() => {
+    if (isAuthChecked && isAuthenticated) {
+      router.replace('/transactions/expenses');
+    }
+  }, [isAuthChecked, isAuthenticated, router]);
+
+  if (!isAuthChecked) {
+    return <Loader fullPage />;
+  }
+
+  if (isAuthenticated) {
+    return null;
+  }
+
   return <WelcomePage />;
 }
